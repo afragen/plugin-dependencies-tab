d79c6c2ff914ddbe1db9ce62cd5731690afdf6a4
Andy Fragen <andy@thefragens.com>
2022-02-15 15:57:49 -0800

    update tests

--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@
  * Plugin URI: https://github.com/afragen/plugin-dependencies-tab
  * Description: Parses 'Requires Plugin' header, add plugin install dependencies tab, and information about dependencies.
  * Author: Andy Fragen
- * Version: 0.11.2
+ * Version: 0.11.3
  * License: MIT
  * Network: true
  * Requires at least: 5.1
--- tests/test-plugin-dependencies.php
+++ tests/test-plugin-dependencies.php
@@ -1,21 +1,46 @@
 <?php
+
 /**
  * Class PluginDependencies
  *
  * @package Plugin_Dependencies_Tab
  */
 
+use WP_Plugin_Dependencies as Dependencies;
 
 /**
  * Sample test case.
  */
-class PluginDependencies extends WP_UnitTestCase {
+class PluginDependencies extends WP_UnitTestCase
+{
 
 	/**
 	 * A single example test.
 	 */
-	public function test_sample() {
+	public function test_sample()
+	{
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$this->assertTrue(true);
+	}
+
+	public function slug_data()
+	{
+		return [
+			[['test' => ['RequiresPlugins' => 'hello-dolly']], ['hello-dolly']],
+			[['test2' => ['RequiresPlugins' => 'hello-dolly, woocommerce']], ['hello-dolly', 'woocommerce']],
+			[['test3' => ['RequiresPlugins' => 'woocommerce, hello-dolly']], ['hello-dolly', 'woocommerce']],
+			[['test4' => ['RequiresPlugins' => 'hello-dolly,gutenberg,  "junk-list , here for test", 435_bad']], ['gutenberg', 'hello-dolly']],
+		];
+	}
+
+	/**
+	 * @dataProvider slug_data
+	 *
+	 * @return void
+	 */
+	public function test_slug_sanitization($headers, $expected)
+	{
+		$actual = (new Dependencies)->sanitize_required_headers($headers);
+		$this->assertSame($expected, $actual);
 	}
 }
