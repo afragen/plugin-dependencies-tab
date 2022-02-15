cf6c5bf1484cef5ac61d1c0640b2315766ed186d
Andy Fragen <andy@thefragens.com>
2022-02-15 12:59:42 -0800

    add testing scaffold

---
+++ tests/test-plugin-dependencies.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class PluginDependencies
+ *
+ * @package Plugin_Dependencies_Tab
+ */
+
+
+/**
+ * Sample test case.
+ */
+class PluginDependencies extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
