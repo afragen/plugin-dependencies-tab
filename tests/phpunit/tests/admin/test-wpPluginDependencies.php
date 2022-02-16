bc6e95482af143f0ca9ae60649fc9d092a4e323f
Andy Fragen <andy@thefragens.com>
2022-02-15 21:01:16 -0800

    rename test sorta like in wp-develop

--- tests/phpunit/tests/admin/test-wpPluginDependencies.php
+++ tests/phpunit/tests/admin/test-wpPluginDependencies.php
@@ -11,7 +11,7 @@ use WP_Plugin_Dependencies as Dependencies;
 /**
  * Sample test case.
  */
-class PluginDependencies extends WP_UnitTestCase
+class wpPluginDependencies extends WP_UnitTestCase
 {
 
 	/**
