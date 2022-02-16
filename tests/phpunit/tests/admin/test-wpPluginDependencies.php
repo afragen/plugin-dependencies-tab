b095186e4ac240c1132d1c27b8bf5365192b67a4
Andy Fragen <andy@thefragens.com>
2022-02-16 08:09:15 -0800

    lint

--- tests/phpunit/tests/admin/test-wpPluginDependencies.php
+++ tests/phpunit/tests/admin/test-wpPluginDependencies.php
@@ -11,26 +11,29 @@ use WP_Plugin_Dependencies as Dependencies;
 /**
  * Sample test case.
  */
-class wpPluginDependencies extends WP_UnitTestCase
-{
+class wpPluginDependencies extends WP_UnitTestCase {
+
 
 	/**
 	 * A single example test.
 	 */
-	public function test_sample()
-	{
-		// Replace this with some actual testing code.
-		$this->assertTrue(true);
+	public function test_sample() {
+		 // Replace this with some actual testing code.
+		$this->assertTrue( true );
 	}
 
-	public function slug_data()
-	{
-		return [
-			[['test' => ['RequiresPlugins' => 'hello-dolly']], ['hello-dolly']],
-			[['test2' => ['RequiresPlugins' => 'hello-dolly, woocommerce']], ['hello-dolly', 'woocommerce']],
-			[['test3' => ['RequiresPlugins' => 'woocommerce, hello-dolly']], ['hello-dolly', 'woocommerce']],
-			[['test4' => ['RequiresPlugins' => 'hello-dolly,gutenberg,  "junk-list , here for test", 435_bad']], ['gutenberg', 'hello-dolly']],
-		];
+	/**
+	 * Test data.
+	 *
+	 * @return void
+	 */
+	public function slug_data() {
+		return array(
+			array( array( 'test' => array( 'RequiresPlugins' => 'hello-dolly' ) ), array( 'hello-dolly' ) ),
+			array( array( 'test2' => array( 'RequiresPlugins' => 'hello-dolly, woocommerce' ) ), array( 'hello-dolly', 'woocommerce' ) ),
+			array( array( 'test3' => array( 'RequiresPlugins' => 'woocommerce, hello-dolly' ) ), array( 'hello-dolly', 'woocommerce' ) ),
+			array( array( 'test4' => array( 'RequiresPlugins' => 'hello-dolly,gutenberg,  "junk-list , here for test", 435_bad' ) ), array( 'gutenberg', 'hello-dolly' ) ),
+		);
 	}
 
 	/**
@@ -38,10 +41,9 @@ class wpPluginDependencies extends WP_UnitTestCase
 	 *
 	 * @return void
 	 */
-	public function test_slug_sanitization($headers, $expected)
-	{
-		$actual = (new Dependencies())->sanitize_required_headers($headers);
-		$this->assertSame($expected, $actual);
+	public function test_slug_sanitization( $headers, $expected ) {
+		$actual = ( new Dependencies() )->sanitize_required_headers( $headers );
+		$this->assertSame( $expected, $actual );
 	}
 
 }
